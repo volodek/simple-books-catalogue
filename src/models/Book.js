3b009b209d8ccce7d096314b9ef9077c57e21c4a
Vladislav Zhelezovsky <vladislav.zhelezovsky@gmail.com>
2026-02-28 19:18:27 +0300

    Change palceholder picture size

--- src/models/Book.js
+++ src/models/Book.js
@@ -1,10 +1,10 @@
-class Book {
+export class Book {
   constructor(title, author_name, first_publish_year, cover_i) {
     this.title = title;
     this.author_name = author_name.join(", ");
     this.first_publish_year = first_publish_year;
     this.cover_url = cover_i
       ? `https://covers.openlibrary.org/b/id/${cover_i}.jpg`
-      : "https://placehold.jp/180x380.png?text=no+cover";
+      : "https://placehold.jp/180x300.png?text=no+cover";
   }
 }
