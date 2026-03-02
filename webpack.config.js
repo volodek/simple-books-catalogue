b62d2ae457b39a75414e4ee7373afb156b70e36a
Vladislav Zhelezovsky <vladislav.zhelezovsky@gmail.com>
2026-03-02 12:00:37 +0300

    Update webpack config to work both with dev and prod

--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,49 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 
-module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: './',
-    clean: true,
-  },
-  devServer: {
-    static: './dist',
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      minify: true,
-    }),
-    new CopyPlugin({
-      patterns: [
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: './src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+      publicPath: isProduction ? './' : '/',
+      clean: true,
+    },
+    devServer: {
+      static: './dist',
+      hot: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        minify: true,
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src/icons'),
+            to: path.resolve(__dirname, 'dist/assets/icons')
+          },
+        ],
+      }),
+    ],
+    module: {
+      rules: [
         {
-          from: path.resolve(__dirname, 'src/icons'),
-          to: path.resolve(__dirname, 'dist/assets/icons')
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/[name][ext]'
+          }
         },
       ],
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/[name][ext]'
-        }
-      },
-    ],
-  },
-};
+    },
+  }
+}
