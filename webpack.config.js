a7ab790dda4a301da53484827fb80acf87432f77
Vladislav Zhelezovsky <vladislav.zhelezovsky@gmail.com>
2026-03-02 07:37:59 +0300

    Edit webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
@@ -16,6 +17,15 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      minify: true,
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'src/icons'),
+          to: path.resolve(__dirname, 'dist/assets/icons')
+        },
+      ],
     }),
   ],
   module: {
@@ -23,6 +33,13 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]'
+        }
       },
     ],
   },
