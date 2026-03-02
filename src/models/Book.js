327af1e902e63a41255bebd505c3306bbd918c1f
Vladislav Zhelezovsky <vladislav.zhelezovsky@gmail.com>
2026-03-02 08:15:16 +0300

    Handle empty author_name and first_publish_year fields

--- src/models/Book.js
+++ src/models/Book.js
@@ -2,8 +2,8 @@ export class Book {
   constructor(key, title, author_name, first_publish_year, cover_i) {
     this.id = key;
     this.title = title;
-    this.author_name = author_name.join(", ");
-    this.first_publish_year = first_publish_year;
+    this.author_name = author_name?.join(", ") ?? "Unknown author";
+    this.first_publish_year = first_publish_year || "N/A";
     this.cover_url = cover_i
       ? `https://covers.openlibrary.org/b/id/${cover_i}.jpg`
       : "https://placehold.jp/180x300.png?text=no+cover";
